49dc471c0037f54eb512a9031dab995cb6cd7640
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-09-18 10:58:21 -0300

    login e cadastro

--- src/pages/Register/components/RegisterClinicContinue.jsx
+++ src/pages/Register/components/RegisterClinicContinue.jsx
@@ -8,12 +8,21 @@ function RegisterClinicContinue(){
     return(
        <div className="register">
         < Header/>
+        <div className="register_ii">
         <h1 className='registre'>Cadastre sua Clínica</h1>
+        <div class="image-upload-container">
+        <input type="file" id="fileInput" accept="image/*"></input>
+        <label for="fileInput" class="add-button">+</label>
+    </div>
        <div className="clinic_description">
        <p className="registration_information">Descrição: *</p>
        <textarea id='description_clinic'></textarea>
        </div>
+       <div className="register_button">
+        <button className='continue_register'>Cadastrar</button>
        </div>
+       </div>
+        </div>
     );
 }
 
