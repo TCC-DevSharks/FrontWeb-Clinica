813dc876213c96698209ab3d28c727262bb2f82b
Ingryd Shirlley <ingryddasilva7@gmail.com>
2023-09-28 11:02:44 -0300

    responsividade

--- src/pages/Login/components/PregnantLogin.jsx
+++ src/pages/Login/components/PregnantLogin.jsx
@@ -3,22 +3,23 @@ import React from 'react';
 import Header from '../../Main/components/Header'
 
 import '../styles/Login.css';
-import '../styles/PregnantLogin.css'; 
+import '../styles/PregnantLogin.css';
 
-function PregnantLogin(){
-    return(
+function PregnantLogin() {
+    return (
         <div>
-        <Header/>
-        <div className="p_login_container">
-           <div className="dowload_app">
-           <h4>Disponível apenas na versão app para uma melhor experiência :)</h4>
-            <img className='download' src="./images/download.png" alt="" />
-           </div>
-            <div className="phone">
-                <img src="./images/app_mobile.png" alt="" />
+            <Header />
+            <div className="p_login_container">
+                <div className="dowload_app">
+                    <h4>Disponível apenas na versão app para uma <h4>melhor experiência.</h4></h4>
+                    <img className='download' src="./images/download.png" alt="" />
+                </div>
+
+                <div className="phone">
+                    <img src="./images/app_mobile.png" alt="" />
+                </div>
             </div>
         </div>
-       </div>
     );
 };
 
